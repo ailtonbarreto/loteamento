e940d04ee3a9c94c3c08f1374e0e928461eb9640
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2026-09-05 16:07:13 -0300

    Update menu.js

--- home/js/menu.js
+++ home/js/menu.js
@@ -2,26 +2,26 @@ const sidebar = document.getElementById("menu");
 const toggleBtn = document.querySelector("#menu-toggle");
 const closeBtn = document.querySelector(".close-menu");
 
-// Abre sidebar
+
 toggleBtn.addEventListener("click", () => {
     sidebar.classList.add("open");
 });
 
-// Fecha sidebar + recolhe submenus
+
 closeBtn.addEventListener("click", () => {
     sidebar.classList.remove("open");
 
-    // Recolhe todos os submenus abertos
+  
     document.querySelectorAll(".has-submenu.open").forEach(item => {
         item.classList.remove("open");
     });
 });
 
-// Controle dos submenus
+
 document.querySelectorAll(".has-submenu").forEach(item => {
     item.addEventListener("click", () => {
 
-        // Fecha outros submenus antes de abrir o atual
+
         document.querySelectorAll(".has-submenu.open").forEach(openItem => {
             if (openItem !== item) {
                 openItem.classList.remove("open");
